3879adeeab7ad7250763b8735b0800113511142b
Kinzzmunzz <122981248+Kinzzmunzz@users.noreply.github.com>
2023-11-03 21:20:40 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Hi chào cọu,tớ coá điều mún nói với cọu á. </br> Làm mẹ tớ nhé !",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
+    "Hãy trả lời từ tận đáy ruột già nhưng k phải nơi đấy=)). Nếu cọu im lặng và rời đi thì tức là cọu đã ĐỒNG Ý.",
   buttonYes: "Tớ đồng ý",
   buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+  titleModar: "Yehh Mama",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "ok mama=))))). </br> Vcl tự dưng có mama 2 phẩy=)) .</br> Mặc váy vào đi tối nay chở sugar mama đi chơi .</br> LoVe:).",
 };
